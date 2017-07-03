b273de76616ee273da93b8ca2027f59939fa9b3a
Minn Soe <contributions@minn.io>
2017-07-04 00:56:51 +0100

    Drop module check and format code

--- iypm_domain.py
+++ iypm_domain.py
@@ -1,38 +1,24 @@
-import sys
-
-try:
-    from troposphere import Sub, Output, Export
-    from troposphere import Parameter, Ref, Template
-    from troposphere.route53 import HostedZone
-except ImportError:
-    sys.exit('Unable to import troposphere. '
-             'Try "pip install troposphere[policy]".')
-
+from troposphere import (Sub, Output, Export, Parameter, Ref, Template)
+from troposphere.route53 import HostedZone
 
 t = Template()
 
-
 t.add_description('Template for creating the IYPM DNS Zone on Route53.')
 
-
-zone_name = t.add_parameter(Parameter(
-    'ZoneName',
-    Description='The name of the DNS Zone to create (example.com).',
-    Type='String'
-))
-
+zone_name = t.add_parameter(
+    Parameter(
+        'ZoneName',
+        Description='The name of the DNS Zone to create (example.com).',
+        Type='String'))
 
 hosted_zone = t.add_resource(HostedZone('DNSZone', Name=Ref(zone_name)))
-
 
 t.add_output([
     Output(
         'ZoneId',
         Description='Route53 Zone ID',
         Value=Ref(hosted_zone),
-        Export=Export(Sub('${AWS::StackName}-R53Zone'))
-    ),
+        Export=Export(Sub('${AWS::StackName}-R53Zone'))),
 ])
-
 
 print(t.to_json())
--- iypm_service.py
+++ iypm_service.py
@@ -1,27 +1,17 @@
-import sys
-
-try:
-    from troposphere import GetAtt, Join, Sub, Output, FindInMap
-    from troposphere import Parameter, Ref, Template, ImportValue
-    from troposphere.cloudfront import Distribution, DistributionConfig
-    from troposphere.cloudfront import Origin, DefaultCacheBehavior
-    from troposphere.cloudfront import ForwardedValues, ViewerCertificate
-    from troposphere.cloudfront import CustomOrigin, Logging, CustomErrorResponse
-    from troposphere.cloudfront import Cookies
-    from troposphere.s3 import Bucket, PublicRead, WebsiteConfiguration
-    from troposphere.s3 import RedirectAllRequestsTo
-    from troposphere.route53 import RecordSetType, AliasTarget
-except ImportError:
-    sys.exit('Unable to import troposphere. '
-             'Try "pip install troposphere[policy]".')
-
+from troposphere import (GetAtt, Join, Sub, Output, FindInMap, Parameter, Ref,
+                         Template, ImportValue)
+from troposphere.cloudfront import (Distribution, DistributionConfig, Origin,
+                                    DefaultCacheBehavior, ForwardedValues,
+                                    ViewerCertificate, CustomOrigin, Logging,
+                                    CustomErrorResponse, Cookies)
+from troposphere.s3 import (Bucket, PublicRead, WebsiteConfiguration,
+                            RedirectAllRequestsTo)
+from troposphere.route53 import (RecordSetType, AliasTarget)
 
 t = Template()
 
-
 t.add_description(
     'Template for creating an AWS stack for serving the IYPM front-end.')
-
 
 t.add_mapping('RegionMap', {
     'us-east-2': {
@@ -83,208 +73,162 @@ t.add_mapping('RegionMap', {
 })
 
 # Parameters
-zone_name = t.add_parameter(Parameter(
-    'ZoneName',
-    Description='The name of the DNS Zone (example.com)',
-    Type='String'
-))
+zone_name = t.add_parameter(
+    Parameter(
+        'ZoneName',
+        Description='The name of the DNS Zone (example.com)',
+        Type='String'))
 
+domain_stack_name = t.add_parameter(
+    Parameter(
+        'DomainStackName',
+        Description='Domain stack which exports a ZoneId',
+        Type='String'))
 
-domain_stack_name = t.add_parameter(Parameter(
-    'DomainStackName',
-    Description='Domain stack which exports a ZoneId',
-    Type='String'
-))
-
-
-acm_cert_arn = t.add_parameter(Parameter(
-    'ACMCertARN',
-    Description='ACM certificate ARN covering the desired DNS zone',
-    Type='String'
-))
-
+acm_cert_arn = t.add_parameter(
+    Parameter(
+        'ACMCertARN',
+        Description='ACM certificate ARN covering the desired DNS zone',
+        Type='String'))
 
 # S3 Buckets
-root_bucket = t.add_resource(Bucket(
-    'RootBucket',
-    BucketName=Sub('${ZoneName}-root'),
-    AccessControl=PublicRead,
-    WebsiteConfiguration=WebsiteConfiguration(
-        IndexDocument='index.html',
-        ErrorDocument='index.html'
-    )
-))
+root_bucket = t.add_resource(
+    Bucket(
+        'RootBucket',
+        BucketName=Sub('${ZoneName}-root'),
+        AccessControl=PublicRead,
+        WebsiteConfiguration=WebsiteConfiguration(
+            IndexDocument='index.html', ErrorDocument='index.html')))
 
+redirect_bucket = t.add_resource(
+    Bucket(
+        'RedirectBucket',
+        BucketName=Sub('${ZoneName}-redirect'),
+        AccessControl=PublicRead,
+        WebsiteConfiguration=WebsiteConfiguration(
+            RedirectAllRequestsTo=RedirectAllRequestsTo(
+                HostName=Ref(zone_name), Protocol='https'), )))
 
-redirect_bucket = t.add_resource(Bucket(
-    'RedirectBucket',
-    BucketName=Sub('${ZoneName}-redirect'),
-    AccessControl=PublicRead,
-    WebsiteConfiguration=WebsiteConfiguration(
-        RedirectAllRequestsTo=RedirectAllRequestsTo(
-            HostName=Ref(zone_name),
-            Protocol='https'
-        ),
-    )
-))
-
-
-log_bucket = t.add_resource(Bucket(
-    'AccessLogBucket',
-    BucketName=Sub('${ZoneName}-log')
-))
-
+log_bucket = t.add_resource(
+    Bucket('AccessLogBucket', BucketName=Sub('${ZoneName}-log')))
 
 # Cloudfront Distributions
-root_distribution = t.add_resource(Distribution(
-    'RootDistribution',
-    DistributionConfig=DistributionConfig(
-        Comment='Cloudfront distribution for zone root',
-        Aliases=[Ref(zone_name)],
-        Origins=[
-            Origin(
-                Id='RootEndpoint',
-                DomainName=Join(
-                    '.',
-                    [
+root_distribution = t.add_resource(
+    Distribution(
+        'RootDistribution',
+        DistributionConfig=DistributionConfig(
+            Comment='Cloudfront distribution for zone root',
+            Aliases=[Ref(zone_name)],
+            Origins=[
+                Origin(
+                    Id='RootEndpoint',
+                    DomainName=Join('.', [
                         Ref(root_bucket),
-                        FindInMap(
-                            'RegionMap',
-                            Ref('AWS::Region'),
-                            'WebsiteEndpoint')
-                    ]
-                ),
-                CustomOriginConfig=CustomOrigin(OriginProtocolPolicy='http-only')
-            )
-        ],
-        DefaultCacheBehavior=DefaultCacheBehavior(
-            Compress=True,
-            CachedMethods=['GET', 'HEAD'],
-            TargetOriginId='RootEndpoint',
-            ForwardedValues=ForwardedValues(
-                QueryString=False,
-                Cookies=Cookies(Forward='none')
-            ),
-            ViewerProtocolPolicy='redirect-to-https'
-        ),
-        CustomErrorResponses=[
-            CustomErrorResponse(
-                ErrorCode=404,
-                ResponseCode=200,
-                ResponsePagePath='/'
-            )
-        ],
-        DefaultRootObject='index.html',
-        Enabled=True,
-        Logging=Logging(
-            Bucket=GetAtt(log_bucket, 'DomainName'),
-            IncludeCookies=True
-        ),
-        PriceClass='PriceClass_All',
-        HttpVersion='http2',
-        ViewerCertificate=ViewerCertificate(
-            AcmCertificateArn=Ref(acm_cert_arn),
-            SslSupportMethod='sni-only'
-        )
-    )
-))
+                        FindInMap('RegionMap',
+                                  Ref('AWS::Region'), 'WebsiteEndpoint')
+                    ]),
+                    CustomOriginConfig=CustomOrigin(
+                        OriginProtocolPolicy='http-only'))
+            ],
+            DefaultCacheBehavior=DefaultCacheBehavior(
+                Compress=True,
+                CachedMethods=['GET', 'HEAD'],
+                TargetOriginId='RootEndpoint',
+                ForwardedValues=ForwardedValues(
+                    QueryString=False, Cookies=Cookies(Forward='none')),
+                ViewerProtocolPolicy='redirect-to-https'),
+            CustomErrorResponses=[
+                CustomErrorResponse(
+                    ErrorCode=404, ResponseCode=200, ResponsePagePath='/')
+            ],
+            DefaultRootObject='index.html',
+            Enabled=True,
+            Logging=Logging(
+                Bucket=GetAtt(log_bucket, 'DomainName'), IncludeCookies=True),
+            PriceClass='PriceClass_All',
+            HttpVersion='http2',
+            ViewerCertificate=ViewerCertificate(
+                AcmCertificateArn=Ref(acm_cert_arn),
+                SslSupportMethod='sni-only'))))
 
-redirect_distribution = t.add_resource(Distribution(
-    'RedirectDistribution',
-    DistributionConfig=DistributionConfig(
-        Comment='Cloudfront distribution for wildcard redirection',
-        Aliases=[Sub('*.${ZoneName}')],
-        Origins=[
-            Origin(
-                Id=Sub('RedirectEndpoint'),
-                DomainName=Join(
-                    '.',
-                    [
+redirect_distribution = t.add_resource(
+    Distribution(
+        'RedirectDistribution',
+        DistributionConfig=DistributionConfig(
+            Comment='Cloudfront distribution for wildcard redirection',
+            Aliases=[Sub('*.${ZoneName}')],
+            Origins=[
+                Origin(
+                    Id=Sub('RedirectEndpoint'),
+                    DomainName=Join('.', [
                         Ref(redirect_bucket),
-                        FindInMap(
-                            'RegionMap',
-                            Ref('AWS::Region'),
-                            'WebsiteEndpoint')
-                    ]
-                ),
-                CustomOriginConfig=CustomOrigin(OriginProtocolPolicy='http-only')
-            )
-        ],
-        DefaultCacheBehavior=DefaultCacheBehavior(
-            Compress=True,
-            CachedMethods=['GET', 'HEAD'],
-            TargetOriginId='RedirectEndpoint',
-            ForwardedValues=ForwardedValues(
-                QueryString=False,
-                Cookies=Cookies(Forward='none')
-            ),
-            ViewerProtocolPolicy='allow-all'
-        ),
-        Enabled=True,
-        PriceClass='PriceClass_All',
-        HttpVersion='http2',
-        ViewerCertificate=ViewerCertificate(
-            AcmCertificateArn=Ref(acm_cert_arn),
-            SslSupportMethod='sni-only'
-        )
-    )
-))
+                        FindInMap('RegionMap',
+                                  Ref('AWS::Region'), 'WebsiteEndpoint')
+                    ]),
+                    CustomOriginConfig=CustomOrigin(
+                        OriginProtocolPolicy='http-only'))
+            ],
+            DefaultCacheBehavior=DefaultCacheBehavior(
+                Compress=True,
+                CachedMethods=['GET', 'HEAD'],
+                TargetOriginId='RedirectEndpoint',
+                ForwardedValues=ForwardedValues(
+                    QueryString=False, Cookies=Cookies(Forward='none')),
+                ViewerProtocolPolicy='allow-all'),
+            Enabled=True,
+            PriceClass='PriceClass_All',
+            HttpVersion='http2',
+            ViewerCertificate=ViewerCertificate(
+                AcmCertificateArn=Ref(acm_cert_arn),
+                SslSupportMethod='sni-only'))))
 
 # Route53 Records
-root_alias_record = t.add_resource(RecordSetType(
-    'RootDistributionAliasRecord',
-    HostedZoneId=ImportValue(
-        Sub('${DomainStackName}-R53Zone')
-    ),
-    Comment='Apex alias record for root distribution',
-    Name=Sub('${ZoneName}.'),
-    Type='A',
-    AliasTarget=AliasTarget(
-        DNSName=GetAtt(root_distribution, 'DomainName'),
-        HostedZoneId='Z2FDTNDATAQYW2'
-    )
-))
+root_alias_record = t.add_resource(
+    RecordSetType(
+        'RootDistributionAliasRecord',
+        HostedZoneId=ImportValue(Sub('${DomainStackName}-R53Zone')),
+        Comment='Apex alias record for root distribution',
+        Name=Sub('${ZoneName}.'),
+        Type='A',
+        AliasTarget=AliasTarget(
+            DNSName=GetAtt(root_distribution, 'DomainName'),
+            HostedZoneId='Z2FDTNDATAQYW2')))
 
-
-wildcard_alias_record = t.add_resource(RecordSetType(
-    'RedirectDistributionAliasRecord',
-    HostedZoneId=ImportValue(
-        Sub('${DomainStackName}-R53Zone')
-    ),
-    Comment='Wildcard alias record for redirect distribution',
-    Name=Sub('*.${ZoneName}.'),
-    Type='A',
-    AliasTarget=AliasTarget(
-        DNSName=GetAtt(redirect_distribution, 'DomainName'),
-        HostedZoneId='Z2FDTNDATAQYW2'
-    )
-))
-
+wildcard_alias_record = t.add_resource(
+    RecordSetType(
+        'RedirectDistributionAliasRecord',
+        HostedZoneId=ImportValue(Sub('${DomainStackName}-R53Zone')),
+        Comment='Wildcard alias record for redirect distribution',
+        Name=Sub('*.${ZoneName}.'),
+        Type='A',
+        AliasTarget=AliasTarget(
+            DNSName=GetAtt(redirect_distribution, 'DomainName'),
+            HostedZoneId='Z2FDTNDATAQYW2')))
 
 t.add_output([
     Output(
         'RootDistributionId',
         Description='Cloudfront distribution ID for root zone',
-        Value=Ref(root_distribution)
-    ),
+        Value=Ref(root_distribution)),
     Output(
         'RootDistributionEndpoint',
         Description='Cloudfront endpoint for root zone',
-        Value=Join('', ['http://', GetAtt(root_distribution, 'DomainName')])),
+        Value=Join('', ['http://',
+                        GetAtt(root_distribution, 'DomainName')])),
     Output(
         'RedirectDistributionId',
         Description='Cloudfront distribution ID for subdomain wildcard redirect',
-        Value=Ref(redirect_distribution)
-    ),
+        Value=Ref(redirect_distribution)),
     Output(
         'RedirectDistributionEndpoint',
         Description='Cloudfront endpoint for subdomain wildcard redirect',
-        Value=Join('', ['http://', GetAtt(redirect_distribution, 'DomainName')])),
+        Value=Join('',
+                   ['http://',
+                    GetAtt(redirect_distribution, 'DomainName')])),
     Output(
         'AccessLogBucket',
         Description='Cloudfront access log S3 bucket ID',
-        Value=Ref(log_bucket)
-    )
+        Value=Ref(log_bucket))
 ])
 
 print(t.to_json())
